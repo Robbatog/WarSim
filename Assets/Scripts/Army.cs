d5bfb331ed9b165f815065f3b08b3533a25ac2e5
Robbatog <robin.ytterlid@gmail.com>
2017-10-29 19:57:43 +0100

    CHG: Can now create default owned army buttons when clicking a recruit army button in ArmyOverviewPanel

--- Assets/Scripts/Army.cs
+++ Assets/Scripts/Army.cs
@@ -1,24 +1,26 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Army {
 
+	public string name;
 	public Unit[,] units;
 	public byte[] spriteBytesPNG;
 
-	public Army(ArmySave armySave)
+	public Army(string armyName, ArmySave armySave)
 	{
-		units = new Unit[GS.armyRowAmount, GS.armyColumnAmount];
+		this.name = armyName;
+		units = new Unit[GS.ArmyRowAmount, GS.ArmyColumnAmount];
 		var dataController = GameObject.Find("DataController").GetComponent<DataController>();
 		for (int i = 0; i < units.GetLength(0); i++)
 		{
 			for (int j = 0; j < units.GetLength(1); j++)
 			{
-				var name = armySave.unitNames[i, j];
-				if (name != "")
+				var uName = armySave.unitNames[i, j];
+				if (uName != "")
 				{
-					units[i, j] = new Unit(dataController.GetUnitData(name));
+					units[i, j] = new Unit(dataController.GetUnitData(uName));
 				}
 			}
 		}
